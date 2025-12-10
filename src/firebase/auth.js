5b5fd8bdf5967fb40e6466b628f3dc0b383ddee6
Mutlu Akgun <78053007+MutluRenegado@users.noreply.github.com>
2025-12-10 14:05:34 +0300

    Update auth.js

--- src/firebase/auth.js
+++ src/firebase/auth.js
@@ -1,1 +1,18 @@
+import { auth, provider } from "./firebase.config";
+import {
+  signInWithEmailAndPassword,
+  createUserWithEmailAndPassword,
+  signOut,
+  signInWithPopup,
+} from "firebase/auth";
+
+export const emailLogin = (email, password) =>
+  signInWithEmailAndPassword(auth, email, password);
+
+export const emailSignup = (email, password) =>
+  createUserWithEmailAndPassword(auth, email, password);
+
+export const googleLogin = () => signInWithPopup(auth, provider);
+
+export const logout = () => signOut(auth);
 

202e6d37392798b8f737546d2795b1026e5216c9
Mutlu Akgun <78053007+MutluRenegado@users.noreply.github.com>
2025-12-10 14:05:56 +0300

    Update firestore.js

--- src/firebase/firestore.js
+++ src/firebase/firestore.js
@@ -1,1 +1,12 @@
+import { db } from "./firebase.config";
+import { doc, setDoc, getDoc } from "firebase/firestore";
+
+export const saveUserData = async (uid, data) => {
+  await setDoc(doc(db, "users", uid), data, { merge: true });
+};
+
+export const getUserData = async (uid) => {
+  const snap = await getDoc(doc(db, "users", uid));
+  return snap.exists() ? snap.data() : null;
+};
 
